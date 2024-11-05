10528835fe1e7083afa6ebb205e2a7e17280f325
Esteban-Gacha <159479229+Esteban-Gacha@users.noreply.github.com>
2024-11-04 20:51:39 -0500

    Add files via upload

---
+++ TuCarroUQ/ProyectoFinal/TuCarroUQ/logicatucarrouq/src/main/java/co/edu/uniquindio/poo/model/App.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
